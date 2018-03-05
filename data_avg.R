6eaacb93ffc9a40ee1759c5fd82d13991cdc19e8
victoria <Owner@victoria.local>
2018-03-04 21:26:25 -0600

    third

--- data_avg.R
+++ data_avg.R
@@ -1,15 +1,15 @@
 install.packages("plyr")
 library(plyr)
 
-x = read.table(file.choose(DataSet),header = TRUE,sep = ",")
-x
-y = ddply(DataSet, "Sex", transform, Grade.Average=mean(Grade))
+x = read.table(file.choose(DataSet),header = TRUE,sep = ",") ##importing the dataset
+x   ##printing the dataset
+y = ddply(DataSet, "Sex", transform, Grade.Average=mean(Grade))  ##sorting by sex and calculating the mean
 y
-write.table(y,"Sorted_Average")
+write.table(y,"Sorted_Average")  ##creating a file for the newly created table
 
-write.table(y,"Sorted_Average", sep = ",")
+write.table(y,"Sorted_Average", sep = ",")  ##seperating the data with commas
 
-newx = subset(x,grepl("[iI]",x$Name))
+newx = subset(x,grepl("[iI]",x$Name))  ##sorting the data to only show names with "i" or "I"
 newx
-write.table(newx,"DataSubSet",sep = ",")
+write.table(newx,"DataSubSet",sep = ",")  ##saving the new table
                  
