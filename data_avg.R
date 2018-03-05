8c73c9af3fd57eb91e1796d12a55347a2c7727ad
victoria <Owner@victoria.local>
2018-03-04 21:21:58 -0600

    second

--- data_avg.R
+++ data_avg.R
@@ -1,6 +1,15 @@
 install.packages("plyr")
 library(plyr)
 
+x = read.table(file.choose(DataSet),header = TRUE,sep = ",")
+x
 y = ddply(DataSet, "Sex", transform, Grade.Average=mean(Grade))
 y
 write.table(y,"Sorted_Average")
+
+write.table(y,"Sorted_Average", sep = ",")
+
+newx = subset(x,grepl("[iI]",x$Name))
+newx
+write.table(newx,"DataSubSet",sep = ",")
+                 
